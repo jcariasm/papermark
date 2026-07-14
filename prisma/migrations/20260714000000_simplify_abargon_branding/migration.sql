77db41bf727ed55600f5bd11eb1b7b5168a3acae
Juan Carias <jcarias@abargon.com>
2026-07-13 20:16:27 -0600

    chore(branding): persist abargon white brand

---
+++ prisma/migrations/20260714000000_simplify_abargon_branding/migration.sql
@@ -0,0 +1,7 @@
+UPDATE "Brand"
+SET
+  "brandColor" = '#FFFFFF',
+  "accentColor" = '#FFFFFF',
+  "accentButtonColor" = '#FFFFFF',
+  "updatedAt" = CURRENT_TIMESTAMP
+WHERE "teamId" = 'cmrh6dxd60000jp04mh7l6y10';
